b8bdffb1bd9aa460d32d7d6dafc52ff3b2f3b675
Matthew Weier O'Phinney <matthew@weierophinney.net>
2019-12-31 11:33:26 -0600

    Rewriting as Laminas Project package

--- test/Controller/TestAsset/ConsoleController.php
+++ test/Controller/TestAsset/ConsoleController.php
@@ -1,13 +1,14 @@
 <?php
+
 /**
- * @link      http://github.com/zendframework/zend-mvc-console for the canonical source repository
- * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
- * @license   http://framework.zend.com/license/new-bsd New BSD License
+ * @see       https://github.com/laminas/laminas-mvc-console for the canonical source repository
+ * @copyright https://github.com/laminas/laminas-mvc-console/blob/master/COPYRIGHT.md
+ * @license   https://github.com/laminas/laminas-mvc-console/blob/master/LICENSE.md New BSD License
  */
 
-namespace ZendTest\Mvc\Console\Controller\TestAsset;
+namespace LaminasTest\Mvc\Console\Controller\TestAsset;
 
-use Zend\Mvc\Console\Controller\AbstractConsoleController;
+use Laminas\Mvc\Console\Controller\AbstractConsoleController;
 
 class ConsoleController extends AbstractConsoleController
 {
